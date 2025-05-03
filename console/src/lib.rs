a9d4947f64ba2b5db32ca259bf8281aa4f80b480
Nico Huber <nico.h@gmx.de>
2025-05-03 18:13:57 +0200

    feat(console): Implement an optionally thread-unsafe console guard
    Use the `Mutex` from embassy-sync and a `RefCell` to guard console
    objects from re-entrance. A `thread-unsafe` feature can be used to
    select an unsafe `NoopRawMutex`. The `test` feature is necessary to
    link tests with `critical-section`.
    
    Caveats:
    * If a console sink would try to log something, it would dealock.
      The thread-unsafe version  could simply drop the data when the
      `borrow_mut()` fails.  The thread-safe version, however, would
      have to know if it should stall (another thread is logging) or
      should drop the data (re-entrace on the same thread). Probably
      a case for thread-local storage.
    * The embassy-sync mutex isn't ideal for this synchronous case.
    
    Signed-off-by: Nico Huber <nico.h@gmx.de>

--- console/src/lib.rs
+++ console/src/lib.rs
@@ -1,4 +1,14 @@
 #![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(any(test, feature = "thread-unsafe")),
+            forbid(unsafe_code))]
+
+#[cfg(feature = "thread-unsafe")]
+use embassy_sync::blocking_mutex::raw::NoopRawMutex as RawMutex;
+#[cfg(not(feature = "thread-unsafe"))]
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex as RawMutex;
+
+use embassy_sync::mutex::Mutex;
+
 
 mod sink;
 pub use sink::Sink;
@@ -7,12 +17,39 @@ mod hub;
 
 const MAX_SINKS: usize = 8;
 
+struct Guard<T> {
+    inner: Mutex<RawMutex, T>
+}
+
+// SAFETY: Fake `Sync` only if we build explicitly without thread safety.
+#[cfg(feature = "thread-unsafe")]
+unsafe impl<T> Sync for Guard<T> { }
+
+impl<T> Guard<T> {
+    const fn new(value: T) -> Self { Self { inner: Mutex::new(value) } }
+
+    fn lock_mut<U>(&self, f: impl FnOnce(&mut T) -> U) -> U {
+        // Block manually on non-blocking mutex.
+        loop {
+            // Console sinks trying to log something would deadlock here.
+            // This could be avoided with some thread-local state, telling
+            // us if the current thread runs console code.
+            match self.inner.try_lock() {
+                Ok(mut val) => {
+                    return f(&mut val);
+                }
+                _ => ()
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use std::vec::Vec;
     use super::*;
 
-    static mut HUB: hub::Hub<MAX_SINKS> = hub::Hub::new();
+    static HUB: Guard<hub::Hub<MAX_SINKS>> = Guard::new(hub::Hub::new());
     static mut SINK: VecSink = VecSink { data: Vec::new() };
 
     struct VecSink {
@@ -27,22 +64,24 @@ mod tests {
 
     #[test]
     fn log_nowhere() {
-        unsafe {
-            #![allow(static_mut_refs)]
-            HUB.write("No sinks added, this warning goes nowhere!".as_bytes());
-            HUB.flush();
-        }
+        HUB.lock_mut(|hub| {
+            hub.write("No sinks added, this warning goes nowhere!".as_bytes());
+            hub.flush();
+        });
     }
 
     #[test]
     fn say_hello() {
         unsafe {
             #![allow(static_mut_refs)]
-            HUB.add(&mut SINK);
-
             let hello = "Say hello through a vector!";
-            HUB.write(hello.as_bytes());
-            HUB.flush();
+
+            HUB.lock_mut(|hub| {
+                hub.add(&mut SINK);
+
+                hub.write(hello.as_bytes());
+                hub.flush();
+            });
 
             assert_eq!(hello.as_bytes(), SINK.data);
         }
