55f39b473a73944812fe74548d2dbd509f19a8bf
Nico Huber <nico.h@gmx.de>
2025-06-15 16:58:29 +0200

    feat(fs): Implement double::Digest
    Signed-off-by: Nico Huber <nico.h@gmx.de>

--- fs/src/crypto/double.rs
+++ fs/src/crypto/double.rs
@@ -1,6 +1,65 @@
+use core::ops::Add;
 use embedded_io_async::Read;
+use generic_array::ArrayLength;
+use digest::{Update, HashMarker, OutputSizeUser,
+             Output, FixedOutput, Reset, FixedOutputReset};
 use signature::{Error, Result, Signer, Verifier};
 use super::ParseSignature;
+
+#[derive(Default)]
+pub struct Digest<D1, D2>(pub D1, pub D2);
+
+impl<D1: FixedOutput, D2: FixedOutput> FixedOutput for Digest<D1, D2>
+where
+    D1::OutputSize: Add<D2::OutputSize>,
+    <D1::OutputSize as Add<D2::OutputSize>>::Output: ArrayLength<u8>,
+{
+    fn finalize_into(self, out: &mut Output<Self>) {
+        let (d1, d2) = out.split_at_mut(D1::output_size());
+        self.0.finalize_into(Output::<D1>::from_mut_slice(d1));
+        self.1.finalize_into(Output::<D2>::from_mut_slice(d2));
+    }
+}
+
+impl<D1, D2> FixedOutputReset for Digest<D1, D2>
+where
+    D1: FixedOutputReset,
+    D2: FixedOutputReset,
+    D1::OutputSize: Add<D2::OutputSize>,
+    <D1::OutputSize as Add<D2::OutputSize>>::Output: ArrayLength<u8>,
+{
+    fn finalize_into_reset(&mut self, out: &mut Output<Self>) {
+        let (d1, d2) = out.split_at_mut(D1::output_size());
+        self.0.finalize_into_reset(Output::<D1>::from_mut_slice(d1));
+        self.1.finalize_into_reset(Output::<D2>::from_mut_slice(d2));
+    }
+}
+
+impl<D1: Reset, D2: Reset> Reset for Digest<D1, D2> {
+    fn reset(&mut self) {
+        self.0.reset();
+        self.1.reset();
+    }
+}
+
+impl<D1, D2> OutputSizeUser for Digest<D1, D2>
+where
+    D1: OutputSizeUser,
+    D2: OutputSizeUser,
+    D1::OutputSize: Add<D2::OutputSize>,
+    <D1::OutputSize as Add<D2::OutputSize>>::Output: ArrayLength<u8>,
+{
+    type OutputSize = <D1::OutputSize as Add<D2::OutputSize>>::Output;
+}
+
+impl<D1: Update, D2: Update> Update for Digest<D1, D2> {
+    fn update(&mut self, data: &[u8]) {
+        self.0.update(data);
+        self.1.update(data);
+    }
+}
+
+impl<D1, D2> HashMarker for Digest<D1, D2> { }
 
 pub struct VerifyingKey<V1, V2>(pub V1, pub V2);
 
